9d7db16bb3cbb36ade54167b9697181a3baa273e
andresgonzalez03 <2023_andres.gonzalez@iticbcn.cat>
2024-10-09 13:21:05 +0200

    encara no acabat monoalfabetic, dubtes si el alfabet ha de permutar cada vegada que s'executa el programa o no

--- 03-Monoalfa/Monoalfabetic.java
+++ 03-Monoalfa/Monoalfabetic.java
@@ -1,17 +1,16 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Monoalfabetic {
     private static final char[] ALFABET = "AÁÀÄBCÇDEÉÈËFGHIÍÌÏJKLMNÑOÓÒÖPQRSTUÚÙÜXYZ".toCharArray();
     public static void main(String args[]) {
         System.out.println(permutaAlfabet(ALFABET));
         System.out.println(Arrays.toString(permutaAlfabet(ALFABET)));
+        System.out.println(xifraMonoAlfa("Hola"));
     }
 
     public static char[] permutaAlfabet(char[] alfabet) {
         char[] alfabetPermutat = new char[alfabet.length];
-        List<Character> permutat = Arrays.asList(convertirACharacter(alfabet));
+        List<Character> permutat = new ArrayList<>(Arrays.asList(convertirACharacter(alfabet)));
         Collections.shuffle(permutat);
         for (int j = 0; j < permutat.size(); j++) {
             alfabetPermutat[j] = permutat.get(j);
@@ -26,27 +25,49 @@ public class Monoalfabetic {
         }
         return alfabetCharacter;  
     }
-    public String xifraMonoAlfa(String cadena) {
+    public static String xifraMonoAlfa(String cadena) {
         StringBuilder sb = new StringBuilder();
         char[] cadenaArray =  permutaAlfabet(cadena.toCharArray());
-        for(int i = 0; i < cadena.length(); i++) {
-            char c = cadena.charAt(i);
+        char[] cadenaNormal = cadena.toCharArray();
+        for(int i = 0; i < cadenaNormal.length; i++) {
+            char c = cadenaNormal[i];
             if(Character.isLetter(c)) {
-                sb.append(Character.toUpperCase(cadenaArray[i]));
-                
-            } else {
-                sb.append(c);
-            }
-        }
-        for(int i = 0; i < cadena.length(); i++) {
-            char c = cadena.charAt(i);
-            if(Character.isLetter(c)) {
-                sb.append(Character.toLowerCase(cadenaArray[i]));
-                
+                if (Character.isUpperCase(c)) {
+                    sb.append(Character.toUpperCase(cadenaArray[i]));
+                } else {
+                    sb.append(Character.toLowerCase(cadenaArray[i]));
+                }
             } else {
                 sb.append(c);
             }
         }
         return sb.toString();
     } 
+    public static String desxifraMonoAlfa(String cadena) {
+        StringBuilder sb = new StringBuilder();
+        char[] cadenaArray =  permutaAlfabet(cadena.toCharArray());
+        char[] cadenaNormal = cadena.toCharArray();
+        for(int i = 0; i<cadena.length(); i++) {
+            char c = cadenaNormal[i];
+            if(Character.isLetter(c)) {
+                char descifrat = ' ';
+                for (int j = 0; j < cadenaArray.length; j++) {
+                    char d = cadenaArray[j];
+                    if (Character.toUpperCase(c) == Character.toUpperCase(d)) {
+                        if (Character.isUpperCase(c)) {
+                            descifrat = d;
+                        } else {
+                            descifrat = Character.toLowerCase(d);
+                        }
+                        break;
+                    }
+                }
+                sb.append(descifrat);
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
 }
+
