b4d7339be52739cd3f1e79a40ac1280325da655a
andresgonzalez03 <2023_andres.gonzalez@iticbcn.cat>
2024-10-10 09:43:02 +0200

    actualitzat main de proves, fet més senzill i òptim

--- 03-Monoalfa/Monoalfabetic.java
+++ 03-Monoalfa/Monoalfabetic.java
@@ -4,35 +4,13 @@ public class Monoalfabetic {
     private static final char[] ALFABET = "AÁÀÄBCÇDEÉÈËFGHIÍÌÏJKLMNÑOÓÒÖPQRSTUÚÙÜVWXYZ".toCharArray();
     private static char[] alfabetPermutat;
     public static void main(String args[]) {
-        String missatge = "Hola, como estás me llamo andrés.";
-        String missatgePermutat = xifraMonoAlfa(missatge);
-        System.out.println("Missatge xifrat: " + missatgePermutat);
-        System.out.println("Missatge desxifrat: " + desxifraMonoAlfa(missatgePermutat));
-        String missatge2 = "?????? qué haces??? visca barça";
-        String missatgePermutat2 = xifraMonoAlfa(missatge2);
-        System.out.println("Missatge xifrat: " + missatgePermutat2);
-        System.out.println("Missatge desxifrat: " + desxifraMonoAlfa(missatgePermutat2));
-        String missatge3 = "667392232 ese es mi número de teléfono.";
-        String missatgePermutat3 = xifraMonoAlfa(missatge3);
-        System.out.println("Missatge xifrat: " + missatgePermutat3);
-        System.out.println("Missatge desxifrat: " + desxifraMonoAlfa(missatgePermutat3));
-        String missatge4 = "Me encantan los buñuelos de bacalao; A mi también jajajaja :))";
-        String missatgePermutat4 = xifraMonoAlfa(missatge4);
-        System.out.println("Missatge xifrat: " + missatgePermutat4);
-        System.out.println("Missatge desxifrat: " + desxifraMonoAlfa(missatgePermutat4));
-        String missatge5 = "àáááááä probando letras ññññçççç pingüino ñandú";
-        String missatgePermutat5 = xifraMonoAlfa(missatge5);
-        System.out.println("Missatge xifrat: " + missatgePermutat5);
-        System.out.println("Missatge desxifrat: " + desxifraMonoAlfa(missatgePermutat5));
-        String missatge6 = "para instalar un .deb tienes que hacer: sudo dpkg -i package_file.deb";
-        String missatgePermutat6= xifraMonoAlfa(missatge6);
-        System.out.println("Missatge xifrat: " + missatgePermutat6);
-        System.out.println("Missatge desxifrat: " + desxifraMonoAlfa(missatgePermutat6));
-        String missatge7 = "linga guli guli guli wacha lingan gu lingan gu";
-        String missatgePermutat7 = xifraMonoAlfa(missatge7);
-        System.out.println("Missatge xifrat: " + missatgePermutat7);
-        System.out.println("Missatge desxifrat: " + desxifraMonoAlfa(missatgePermutat7));
-
+        String[] prova = {"Hola, como estás me llamo andrés.", "?????? qué haces??? visca barça", "667392232 ese es mi número de teléfono.", 
+                        "Me encantan los buñuelos de bacalao; A mi también jajajaja :))","àáááááä probando letras ññññçççç pingüino ñandú",
+                        "para instalar un .deb tienes que hacer: sudo dpkg -i package_file.deb","linga guli guli guli wacha lingan gu lingan gu"};
+        for (String s : prova) {
+            System.out.println("Missatge xifrat: " + xifraMonoAlfa(s));
+            System.out.println("Missatge desxifrat: " + desxifraMonoAlfa(xifraMonoAlfa(s)));
+        }
     }
 
     public static char[] permutaAlfabet(char[] alfabet) {
